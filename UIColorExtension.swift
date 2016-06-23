0ee0ee8cb7c89e104ffbc19bb2772cde04113ce0
gnoell <nathanblamires@gmail.com>
2016-06-23 16:32:12 +1000

    Improved naming

--- UIColorExtension.swift
+++ UIColorExtension.swift
@@ -11,22 +11,22 @@
 
 import UIKit
 
+// MARK: Lightening & Darkening
+
+// Description:
+// This extension provides methods to lighten and darken a UIColor.
+
 extension UIColor {
 	
-	// MARK: Lightening & Darkening
-	
-	// Description:
-	// This extension provides methods to lighten and darken a UIColor.
-	
-	func lightenColour() -> UIColor {
-		return lightenColourByPercentage(0.2)
+	func lighten() -> UIColor {
+		return lightenByPercentage(0.2)
 	}
 	
-	func darkenColour() -> UIColor {
-		return darkenColourByPercentage(0.2)
+	func darken() -> UIColor {
+		return darkenByPercentage(0.2)
 	}
 	
-    func lightenColourByPercentage(percentage: CGFloat) -> UIColor {
+    func lightenByPercentage(percentage: CGFloat) -> UIColor {
 		
 		// extract colour components
 		var oldRed: CGFloat = 0, oldGreen: CGFloat = 0, oldBlue: CGFloat = 0, oldAlpha: CGFloat = 0
@@ -39,7 +39,7 @@ extension UIColor {
         return UIColor(red: red, green: green, blue: blue, alpha: oldAlpha)
     }
     
-    func darkenColourByPercentage(factor: CGFloat) -> UIColor {
+    func darkenByPercentage(factor: CGFloat) -> UIColor {
 		
 		// extract colour components
 		var oldRed: CGFloat = 0, oldGreen: CGFloat = 0, oldBlue: CGFloat = 0, oldAlpha: CGFloat = 0
@@ -51,13 +51,16 @@ extension UIColor {
 		let blue = oldBlue - (factor * oldBlue)
 		return UIColor(red: red, green: green, blue: blue, alpha: oldAlpha)
     }
+}
+
+// MARK: Colour Image
+
+// Description:
+// This extension enables you to get an image from a UIColor.
+
+extension UIColor {
 	
-	// MARK: Colour Image
-	
-	// Description:
-	// This extension enables you to get an image from a UIColor.
-	
-    func colourAsImage() -> UIImage {
+    func asImage() -> UIImage {
         UIGraphicsBeginImageContext(CGSizeMake(1,1))
         let context: CGContextRef = UIGraphicsGetCurrentContext()!
         CGContextSetFillColorWithColor(context, self.CGColor)
