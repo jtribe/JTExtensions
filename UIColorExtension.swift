319fab4eec070809f5f0c60018b8710b37101011
gnoell <nathanblamires@gmail.com>
2016-06-24 09:11:02 +1000

    Fixed indentation

--- UIColorExtension.swift
+++ UIColorExtension.swift
@@ -1,13 +1,13 @@
 
 /*
- * Jtribe
- * UIColorExtension
- *
- * Description:
- * Extensions to make dealing with instances of UIColor more simple, intuative and powerful.
- *
- * Jtribe Copyright © 2016
- */
+* Jtribe
+* UIColorExtension
+*
+* Description:
+* Extensions to make dealing with instances of UIColor more simple, intuative and powerful.
+*
+* Jtribe Copyright © 2016
+*/
 
 import UIKit
 
@@ -26,31 +26,31 @@ extension UIColor {
 		return darkenByPercentage(0.2)
 	}
 	
-    func lightenByPercentage(percentage: CGFloat) -> UIColor {
+	func lightenByPercentage(percentage: CGFloat) -> UIColor {
 		
 		// extract colour components
 		var oldRed: CGFloat = 0, oldGreen: CGFloat = 0, oldBlue: CGFloat = 0, oldAlpha: CGFloat = 0
-        getRed(&oldRed, green: &oldGreen, blue: &oldBlue, alpha: &oldAlpha)
+		getRed(&oldRed, green: &oldGreen, blue: &oldBlue, alpha: &oldAlpha)
 		
 		// form new colour components
 		let red = oldRed + (percentage * (1-oldRed))
 		let green = oldGreen + (percentage * (1-oldGreen))
 		let blue = oldBlue + (percentage * (1-oldBlue))
-        return UIColor(red: red, green: green, blue: blue, alpha: oldAlpha)
-    }
-    
-    func darkenByPercentage(factor: CGFloat) -> UIColor {
+		return UIColor(red: red, green: green, blue: blue, alpha: oldAlpha)
+	}
+	
+	func darkenByPercentage(factor: CGFloat) -> UIColor {
 		
 		// extract colour components
 		var oldRed: CGFloat = 0, oldGreen: CGFloat = 0, oldBlue: CGFloat = 0, oldAlpha: CGFloat = 0
 		getRed(&oldRed, green: &oldGreen, blue: &oldBlue, alpha: &oldAlpha)
-
+		
 		// form new colour components
 		let red = oldRed - (factor * oldRed)
 		let green = oldGreen - (factor * oldGreen)
 		let blue = oldBlue - (factor * oldBlue)
 		return UIColor(red: red, green: green, blue: blue, alpha: oldAlpha)
-    }
+	}
 }
 
 // MARK: Colour Image
@@ -60,13 +60,13 @@ extension UIColor {
 
 extension UIColor {
 	
-    func asImage() -> UIImage {
-        UIGraphicsBeginImageContext(CGSizeMake(1,1))
-        let context: CGContextRef = UIGraphicsGetCurrentContext()!
-        CGContextSetFillColorWithColor(context, self.CGColor)
-        CGContextFillRect(context, CGRectMake(0.0, 0.0, 1.0, 1.0))
-        let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext();
-        return image;
-    }
+	func asImage() -> UIImage {
+		UIGraphicsBeginImageContext(CGSizeMake(1,1))
+		let context: CGContextRef = UIGraphicsGetCurrentContext()!
+		CGContextSetFillColorWithColor(context, self.CGColor)
+		CGContextFillRect(context, CGRectMake(0.0, 0.0, 1.0, 1.0))
+		let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()
+		UIGraphicsEndImageContext();
+		return image;
+	}
 }
